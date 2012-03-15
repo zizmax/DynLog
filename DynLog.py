87c574ad84cbfcc996246fd99686c939a40365f2
Zizma <4u2njoy.nathan@gmail.com>
2012-03-14 22:18:16 -0600

    Support for bugs and removed features.

--- DynLog.py
+++ DynLog.py
@@ -2,13 +2,27 @@ import time
 from time import strftime
 print('Hey and welcome to DynamicLog! V: 1.0 (Bug free!)')
 time.sleep(1)
+# If you don't want to have to enter your name and email each time just...
+# change the variables to look like "name = '(your name)'"
 name = input('Your full name: ') + ' '
 email = '<' + input('Your email: ') + '>'
 date = strftime('%Y-%m-%d ')
 changes = {}
 x = 0
 while x != -1:
-    changes[x] = '* ' + input('Change: ')
+    changes[x] = '+ ' + input('New features: ')
+    if changes[x] == '+ ':
+        del changes[x]
+        break
+    x += 1
+while x != -1:
+    changes[x] = '- ' + input('Removed features: ')
+    if changes[x] == '- ':
+        del changes[x]
+        break
+    x += 1
+while x != -1:
+    changes[x] = '* ' + input('Bugs: ')
     if changes[x] == '* ':
         del changes[x]
         break
